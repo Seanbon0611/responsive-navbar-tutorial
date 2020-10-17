1624039ce5608067046e789ff076b9a2db81a7c1
Sean Dever <45188410+Seanbon0611@users.noreply.github.com>
2020-10-16 23:02:25 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./header/Header";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h3 style={{ padding: "40px" }}>Thanks For Reading!</h3>
+    </div>
+  );
+}
